5e8bd108bd3d42cd6d498367efb7d76d523ccc7f
Tariq Alrefai <Tariq.alrefai96@gmail.com>
2022-07-01 00:18:14 +0300

    compile-jakaroo

--- scripts/compile-Jakaroo.sh
+++ scripts/compile-Jakaroo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir Jakaroo
-
-
-if [ -f ./powersOfTau28_hez_final_12.ptau ]; then
+mkdir circuits/Jakaroo
+cd circuits/Jakaroo
+if [ -f powersOfTau28_hez_final_12.ptau ]; then
     echo "powersOfTau28_hez_final_12.ptau already exists. Skipping."
 else
     echo 'Downloading powersOfTau28_hez_final_12.ptau'
@@ -12,18 +11,20 @@ fi
 
 echo "Compiling Jakaroo.circom..."
 
-# cd circuits
+cd ..
+
 # compile circuit
 
-circom circuits/Jakaroo.circom --r1cs --wasm --sym -o Jakaroo
+circom Jakaroo.circom --r1cs --wasm --sym -o Jakaroo
 snarkjs r1cs info Jakaroo/Jakaroo.r1cs
 
 # Start a new zkey and make a contribution
 
-snarkjs groth16 setup Jakaroo/Jakaroo.r1cs powersOfTau28_hez_final_12.ptau Jakaroo/circuit_0000.zkey
+snarkjs groth16 setup Jakaroo/Jakaroo.r1cs Jakaroo/powersOfTau28_hez_final_12.ptau Jakaroo/circuit_0000.zkey
 snarkjs zkey contribute Jakaroo/circuit_0000.zkey Jakaroo/circuit_final.zkey --name="1st Contributor Name" -v -e="random text"
 snarkjs zkey export verificationkey Jakaroo/circuit_final.zkey Jakaroo/verification_key.json
 
 # generate solidity contract
 snarkjs zkey export solidityverifier Jakaroo/circuit_final.zkey ../JakarooVerifier.sol
 
+cd ..
